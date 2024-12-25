a303b956c596335529e490ea4f258a0bd4912432
AnnaKhadisova <annkhadisova@gmail.com>
2024-12-25 19:07:28 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Robofriends_Anna/",
   plugins: [react()],
+  base:"/Robofriends_Anna/"
 })
